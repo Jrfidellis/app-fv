ea0c0cd0b362c8fb9272ecf6b52088c4e0cf5cea
João Fidellis <joao.fidellis@cwi.com.br>
2020-08-03 00:47:13 -0300

    Adicionando componente de Post 🚏

--- App.tsx
+++ App.tsx
@@ -1,19 +1,27 @@
 import React from 'react';
 import styled from 'styled-components/native'; 
+import { Post } from './app/components/post/Post';
 
 const Texto = styled.Text`
   color: red;
   position: absolute;
   top: 80px;
   left: 20px;
-  font-size: 22px;
+  font-size: 16px;
 `
+const Holder = styled.View`
+  background: #efefef;
+  width: 100%;
+  height: 100%;
+  padding: 100px 20px;
+`;
 
 const App = () => {
   return (
-    <>
+    <Holder>
       <Texto>Teste</Texto>
-    </>
+      <Post/>
+    </Holder>
   );
 };
 
