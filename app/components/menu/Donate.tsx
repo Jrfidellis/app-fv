d8181146efd510e5866c2cba8630512254746095
GabrielZimmmer <gabrielzimmer2011@hotmail.com>
2020-08-04 14:39:19 -0300

    card component

---
+++ app/components/menu/Donate.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { PostCard, Title, Desc, Wrapper } from './About.styles';
+
+export const DonateCard = () => <>
+    <PostCard>
+        <Wrapper>
+            <Title>
+                Doar para o Folego da Vida
+            </Title>
+            <Desc>
+                Doe para suportar os trabalhos do Fôlego de Vida!
+            </Desc>
+        </Wrapper>
+    </PostCard>
+</>
