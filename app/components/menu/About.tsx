17a7ea0aa6efa17993a0d204632e6825439c1484
GabrielZimmmer <gabrielzimmer2011@hotmail.com>
2020-08-04 14:47:54 -0300

    resolvido conflito

---
+++ app/components/menu/About.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { PostCard, Title, Desc, Wrapper } from './About.styles';
+
+export const AboutCard = () => <>
+    <PostCard>
+        <Wrapper>
+            <Title>
+                Sobre nós
+            </Title>
+            <Desc>
+                Informações sobre o Fôlego de Vida e onde você pode nos encontrar!
+            </Desc>
+        </Wrapper>
+    </PostCard>
+</>
--- app/screens/menu.tsx
+++ app/screens/menu.tsx
@@ -2,9 +2,15 @@ import React from 'react';
 import { Text } from 'react-native';
 import styled from 'styled-components/native'; 
 
+import { AboutCard as About } from '../components/menu/About';
+import { DonateCard as Donate } from '../components/menu/Donate'
+
 export const MenuScreen = () => {
   return (
     <Container>
+      <Text>Voce esta no Menu</Text>
+      <Donate/>
+      <About/>
       <Text>Menu</Text>
     </Container>
   );
